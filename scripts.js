ba074e6ffdf86a90e93d94f08fdadd296a6fa187
Besart Elezi <besart.elezi@hotmail.com>
2022-07-26 11:37:32 +0200

    added Pokémon

--- scripts.js
+++ scripts.js
@@ -25,6 +25,19 @@ backgroundLayer5.src = "resources/game-images/05-trees.png";
 const backgroundLayer6 = new Image();
 backgroundLayer6.src = "resources/game-images/06-grass.png";
 
+//adding images of the Pokémon of the trainer
+const torchicImage = new Image();
+torchicImage.src = "resources/game-images/torchic.png";
+const treeckoImage = new Image();
+treeckoImage.src = "resources/game-images/treecko.png";
+const mudkipImage = new Image();
+mudkipImage.src = "resources/game-images/mudkip.png";
+
+//Pokémon typings
+const grass = "grass";
+const fire = "fire";
+const water = "water";
+
 //creating a class so I don't have to write the code for all the different layers
 class ParallaxBackgroundLayer {
     constructor(image, speedModifier) {
@@ -44,11 +57,9 @@ class ParallaxBackgroundLayer {
             //there will always be a gap equal to the gamespeed, and if the canvas_width isn't perfectly divisible by the gameSpeed, another gap will appear
             //This way it won't matter what the gamespeed is, there won't be a gap in between the images in the canvas
             this.x = this.width + this.x2 - this.speed
-            console.log("if 1 works in update")
         }
         if (this.x2 <= -this.width){
             this.x2 = this.width + this.x - this.speed
-            console.log("if 2 works in update")
 
         }
         this.x = Math.floor(this.x - this.speed);
@@ -61,32 +72,58 @@ class ParallaxBackgroundLayer {
     }
 }
 
+class UserPokemon {
+    constructor(image, type, width, height) {
+        this.image = image;
+        this.type = type;
+        this.x = 20;
+        this.y = 300;
+        this.width = width;
+        this.height = height;
+        this.image = image;
+    }
+}
+
+function drawPokemon (img, pokemonX, pokemonY, pokemonWidth, pokemonHeight) {
+    canvasContext.drawImage(img, pokemonX, pokemonY, pokemonWidth, pokemonHeight)
+}
+
+const treecko = new UserPokemon(treeckoImage, grass, 100, 102);
+
+let currentPokemon = treecko;
+
 //creating all the layers in the parallax
 const layerSky = new ParallaxBackgroundLayer(backgroundLayer1, 0.25)
-const layerSea = new ParallaxBackgroundLayer(backgroundLayer2, 1)
+const layerSea = new ParallaxBackgroundLayer(backgroundLayer2, 1.5)
 const layerLapras = new ParallaxBackgroundLayer(backgroundLayer3, 2.5)
-const layerClouds = new ParallaxBackgroundLayer(backgroundLayer4, 0.5)
-const layerTrees = new ParallaxBackgroundLayer(backgroundLayer5, 4.5)
+const layerClouds = new ParallaxBackgroundLayer(backgroundLayer4, 0.25)
+const layerTrees = new ParallaxBackgroundLayer(backgroundLayer5, 3.5)
 const layerGrass = new ParallaxBackgroundLayer(backgroundLayer6, 6)
 
+const layerArray = [layerSky, layerSea, layerLapras, layerClouds, layerTrees, layerGrass]
 
-function animateBackground () {
+function animate () {
     //clears prior drawing on the canvas
     canvasContext.clearRect(0, 0,  CANVAS_WIDTH, CANVAS_HEIGHT)
+    //call on the update and draw functions on each of the layers
+    layerArray.forEach(object => {
+        object.update();
+        object.draw();
+    })
+    drawPokemon(currentPokemon.image, currentPokemon.x, currentPokemon.y, currentPokemon.width, currentPokemon.height)
 
-    //calls all the functions on all the layers
-    layerSky.update();
-    layerSky.draw();
-    layerSea.update();
-    layerSea.draw();
-    layerLapras.update();
-    layerLapras.draw();
-    layerClouds.update();
-    layerClouds.draw();
-    layerTrees.update();
-    layerTrees.draw();
-    layerGrass.update();
-    layerGrass.draw();
-    requestAnimationFrame( animateBackground)
+    requestAnimationFrame(animate)
 }
-animateBackground()
+
+animate()
+
+window.addEventListener("keydown", function (e){
+    console.log(e)
+})
+window.addEventListener("keyup", function (e){
+    console.log(e)
+})
+
+function movePokemon () {
+    
+}
